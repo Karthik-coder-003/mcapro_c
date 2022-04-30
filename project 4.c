971fd958fc9c358d785da836a52d64c230be6315
Karthik-coder-003 <104676363+Karthik-coder-003@users.noreply.github.com>
2022-04-30 16:42:51 +0530

    pizza burger pasta program

--- project 4.c
+++ project 4.c
@@ -13,7 +13,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
-// MYCAPTAIN PROJECT 4
+//  
 #include <stdio.h>
 #include <stdlib.h>
 int main()
